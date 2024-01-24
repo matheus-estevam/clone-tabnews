aa784201ad7cd8113f615ae8513f168494226b72
M(a)theus17 <103658252+matheus-estevam@users.noreply.github.com>
2024-01-24 12:33:48 +0000

    adiciona scripts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-function Home(){
-    return <div>
-        <h1>NEVER GIVE UP</h1>
-        <p>Nunca Vamos Desistir Dos Nossos Sonhos! </p>
+function Home() {
+  return (
+    <div>
+      <h1>NEVER GIVE UP</h1>
+      <p>Nunca Vamos Desistir Dos Nossos Sonhos! </p>
     </div>
+  );
 }
 
 export default Home;
