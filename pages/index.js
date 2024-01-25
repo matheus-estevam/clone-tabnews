2e5bd47257a058ff4000f31ef4849ee0e8c4f0ca
M(a)theus17 <103658252+matheus-estevam@users.noreply.github.com>
2024-01-25 17:32:46 +0000

    fim da fundacao 0

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     <div>
       <h1>NEVER GIVE UP</h1>
       <p>Nunca Vamos Desistir Dos Nossos Sonhos! </p>
+      <p>VAMOS LÁ - OBERSEVAI AS AVES DO CÉU DEUS AS SUSTENTA</p>
     </div>
   );
 }
